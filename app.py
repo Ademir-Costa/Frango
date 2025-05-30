dac312a554033cd61ba7cd28e2e516f0f241fe1c
Ademir Costa <89737928+Ademir-Costa@users.noreply.github.com>
2025-05-29 22:31:44 -0400

    porta de comunicão

--- app.py
+++ app.py
@@ -1023,9 +1023,16 @@ def usuarios_inativos():
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
    #app.run(debug=True)
     
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host="0.0.0.0", port=port)
+    
+    import os
+
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
+
     
