e49dae7ec774ab963a66a198a7d65d25081c52ff
Ademir Costa <89737928+Ademir-Costa@users.noreply.github.com>
2025-05-29 22:29:23 -0400

    Ajuste da porta

--- app.py
+++ app.py
@@ -1026,6 +1026,6 @@ with app.app_context():
 if __name__ == '__main__':
    #app.run(debug=True)
     
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
     
